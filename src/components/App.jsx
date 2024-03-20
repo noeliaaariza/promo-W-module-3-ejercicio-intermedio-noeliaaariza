78ae5e782cc5f6c77623a4ae18dec81e91ab82fd
noeliaariza <noelia.ariza@gmail.com>
2024-03-20 10:50:56 +0100

    running app backup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,37 @@ import Header from "./Header";
 import Filters from "./Filters";
 import AddCountry from "./AddCountry";
 import ListCountries from "./ListCountries";
+import { useState } from "react";
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const [selectValue, setSelectValue] = useState("all");
+
+  const handleChangeSearch = (value) => {
+    setSearchValue(value);
+  };
+
+  const handleChangeSelect = (value) => {
+    setSelectValue(value);
+  };
+
+  const filteredCountries = data.filter((data) => {
+    return data.name.common.toLowerCase().includes(searchValue.toLowerCase());
+  });
+
+  const filteredContinents = data.filter((data) => {
+    return data.continents[0].toLowerCase().includes(selectValue.toLowerCase());
+  });
+
   return (
     <>
       <Header />
-      <Filters />
+      <Filters
+        onChangeInput={handleChangeSearch}
+        onChangeSelect={handleChangeSelect}
+      />
       <AddCountry />
-      <ListCountries countryData={data} />
+      <ListCountries countryData={filteredCountries} />
     </>
   );
 }
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,10 +1,26 @@
-function Filters() {
+function Filters({ onChangeInput, onChangeSelect }) {
+  const handleInput = (ev) => {
+    const inputValue = ev.target.value;
+    onChangeInput(inputValue);
+  };
+
+  const handleSelect = (ev) => {
+    const inputSelectValue = ev.target.value;
+    onChangeSelect(inputSelectValue);
+  };
+
   return (
     <div>
       <label htmlFor="country">By Country</label>
-      <input id="country" type="text" placeholder="Spain..." />
+      <input
+        id="country"
+        type="text"
+        placeholder="Spain..."
+        onChange={handleInput}
+      />
+
       <label htmlFor="continent">By Continent</label>
-      <select id="continent" name="continent">
+      <select id="continent" name="continent" onChange={handleSelect}>
         <option value="all">All</option>
         <option value="africa">Africa</option>
         <option value="northamerica">North America</option>
